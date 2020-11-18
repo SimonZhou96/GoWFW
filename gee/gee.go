1ba9665399890f815ba05235050e054e914e5e25
zhoumeng.simon <zhoumeng.simon@bytedance.com>
2020-11-18 18:50:27 +0800

    抽离出router模块（添加路由，映射路由到执行函数），实现上下文
    Change-Id: I8bbb6e1af644735502f722cce4288fa838e3cc38

--- gee/gee.go
+++ gee/gee.go
@@ -1,36 +1,31 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // HandlerFunc defines the request handler used by gee
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(c *Context)
 
 // Engine implement the interface of ServeHTTP
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 // New is the constructor of gee.Engine
 func NewEngine() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: NewRouter()}
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
-}
 
 // GET defines the method to add GET request
 func (engine *Engine) Get(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+	engine.router.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request
 func (engine *Engine) Post(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+	engine.router.addRoute("POST", pattern, handler)
 }
 
 // Run defines the method to start a http server
@@ -39,10 +34,6 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := NewContext(w, req)
+	engine.router.handle(c)
 }
---
+++ gee/router.go
@@ -0,0 +1,26 @@
+package gee
+
+import "net/http"
+
+type router struct {
+	handlers map[string]HandlerFunc
+}
+
+func NewRouter() *router {
+	return &router{handlers: make(map[string]HandlerFunc)}
+}
+
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	r.handlers[key] = handler
+}
+
+// 路由处理某一个特定的函数
+func (r *router) handle(c *Context) {
+	key := c.Method + "-" + c.Path
+	if handler, ok := r.handlers[key]; ok {
+		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+}
